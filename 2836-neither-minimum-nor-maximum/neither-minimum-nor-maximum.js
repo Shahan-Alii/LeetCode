c772c087092a277d87bc931070d77e1bd0088021
Shahan Ali <shahanali687@gmail.com>
2024-11-15 20:16:15 +0500

    Time: 20 ms (46.05%) | Memory: 59.6 MB (59.26%) - LeetSync

---
+++ 2836-neither-minimum-nor-maximum/neither-minimum-nor-maximum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findNonMinOrMax = function (nums) {
+
+    if (nums.length < 3) {
+        return -1;
+    }
+
+    nums.sort((a, b) => a - b);
+
+    return nums[1];
+
+};
