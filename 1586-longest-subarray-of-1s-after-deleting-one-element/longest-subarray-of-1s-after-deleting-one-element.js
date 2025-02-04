b5babad55523e17defce03d9b577203a1434ca01
Shahan Ali <shahanali687@gmail.com>
2025-02-04 21:37:55 +0500

    Time: 2 ms (68.10%) | Memory: 57.3 MB (14.70%) - LeetSync

---
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestSubarray = function (nums) {
+
+    var max = 0;
+    var count0 = 0;
+    var count1 = 0;
+
+    var start = 0;
+
+    for (var end = 0; end < nums.length; end++) {
+
+        if (nums[end] == 0) {
+            count0++;
+        }
+
+
+        while (count0 > 1) {
+
+            if (nums[start] == 0) {
+                count0--;
+            }
+            start++;
+            max = Math.max(max, end - start)
+        }
+        max = Math.max(max, end - start)
+
+    }
+
+    return max;
+
+};
