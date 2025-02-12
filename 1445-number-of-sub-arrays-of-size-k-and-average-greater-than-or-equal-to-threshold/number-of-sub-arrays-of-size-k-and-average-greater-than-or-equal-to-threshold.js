78728380147bdce622a9a4db32b0bf4aa369337b
Shahan Ali <shahanali687@gmail.com>
2025-02-12 19:29:14 +0500

    Time: 3 ms (35.97%) | Memory: 56.2 MB (90.12%) - LeetSync

---
+++ 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @param {number} threshold
+ * @return {number}
+ */
+var numOfSubarrays = function (arr, k, threshold) {
+
+    let res = 0;
+    let currentSum = 0;
+    let currentAvg = 0.0;
+
+
+    for (let i = 0; i < k; i++) {
+
+        currentSum += arr[i];
+    }
+
+    currentAvg = currentSum / k;
+
+    if (currentAvg >= threshold) { res++ };
+
+
+    for (let i = 0; i < arr.length; i++) {
+
+
+        currentSum -= arr[i];
+        currentSum += arr[i + k];
+
+        currentAvg = currentSum / k;
+
+        if (currentAvg >= threshold) { res++ };
+
+    }
+
+    return res;
+
+};
