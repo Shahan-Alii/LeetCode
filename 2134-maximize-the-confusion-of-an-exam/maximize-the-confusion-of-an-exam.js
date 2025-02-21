6c4649a67a371ab7af638aeaf8b2fbe17f2d3467
Shahan Ali <shahanali687@gmail.com>
2025-02-21 20:42:23 +0500

    Time: 15 ms (78.67%) | Memory: 56.1 MB (10.67%) - LeetSync

---
+++ 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.js
@@ -0,0 +1,48 @@
+/**
+ * @param {string} answerKey
+ * @param {number} k
+ * @return {number}
+ */
+var maxConsecutiveAnswers = function (answerKey, k) {
+
+    let max = 0;
+    let l_true = 0;
+    let l_false = 0;
+
+    let count_true = 0;
+    let count_false = 0;
+
+    for (let right = 0; right < answerKey.length; right++) {
+
+        if (answerKey[right] == "T") {
+            count_true++;
+        } else {
+            count_false++;
+        }
+
+        while (count_false > k) {
+
+            if (answerKey[l_true] == "F") {
+                count_false--;
+            }
+            l_true++;
+
+        }
+
+        while (count_true > k) {
+
+            if (answerKey[l_false] == "T") {
+                count_true--;
+            }
+            l_false++;
+
+        }
+
+        currentMax = Math.max(right - l_true + 1, right - l_false + 1);
+
+        max = Math.max(max, currentMax);
+    }
+
+    return max;
+
+};
