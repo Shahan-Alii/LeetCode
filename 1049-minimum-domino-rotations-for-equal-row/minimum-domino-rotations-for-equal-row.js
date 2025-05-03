396cca6a3219d36845aa322257da4effb3a3c675
Shahan Ali <shahanali687@gmail.com>
2025-05-03 13:26:41 +0500

    Time: 3 ms (83.33%) | Memory: 63.8 MB (72.92%) - LeetSync

--- 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.js
+++ 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.js
@@ -1,26 +1,24 @@
-var minDominoRotations = function (tops, bottoms) {
-    let res = Infinity;
-
-    for (let v = 1; v <= 6; v++) {
-        let top_shifts = 0;
-        let bottom_shifts = 0;
-        let flag = true;
+var minDominoRotations = function(tops, bottoms) {
+    const check = (target) => {
+        let topRotations = 0;
+        let bottomRotations = 0;
 
         for (let i = 0; i < tops.length; i++) {
-            if (tops[i] !== v && bottoms[i] !== v) {
-                flag = false;
-                break;
-            } else if (tops[i] !== v) {
-                top_shifts++;
-            } else if (bottoms[i] !== v) {
-                bottom_shifts++;
+            if (tops[i] !== target && bottoms[i] !== target) {
+                return Infinity;
+            } else if (tops[i] !== target) {
+                topRotations++;
+            } else if (bottoms[i] !== target) {
+                bottomRotations++;
             }
         }
 
-        if (flag) {
-            res = Math.min(res, top_shifts, bottom_shifts);
-        }
-    }
+        return Math.min(topRotations, bottomRotations);
+    };
 
+    const candidate1 = tops[0];
+    const candidate2 = bottoms[0];
+
+    const res = Math.min(check(candidate1), candidate1 !== candidate2 ? check(candidate2) : Infinity);
     return res === Infinity ? -1 : res;
 };
