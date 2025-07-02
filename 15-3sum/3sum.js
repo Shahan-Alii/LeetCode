d102f5c55968261a1bebcccee9815d241e987d68
Shahan Ali <shahanali687@gmail.com>
2025-07-02 15:26:04 +0500

    Time: 39 ms (40.48%) | Memory: 73.1 MB (28.89%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -3,33 +3,45 @@
  * @return {number[][]}
  */
 function threeSum(nums) {
-  nums.sort((a, b) => a - b);          // 1 . sort the array
-  const res = [];
 
-  for (let i = 0; i < nums.length - 2; i++) {
-    if (i > 0 && nums[i] === nums[i - 1]) continue; // skip duplicate anchors
+    const res = [];
 
-    let left = i + 1;
-    let right = nums.length - 1;
+    nums.sort((a, b) => a - b);
 
-    while (left < right) {
-      const sum = nums[i] + nums[left] + nums[right];
+    for (let i = 0; i < nums.length; i++) {
 
-      if (sum === 0) {
-        res.push([nums[i], nums[left], nums[right]]);
+        if (i > 0 && nums[i] === nums[i - 1]) {
+            continue;
+        }
 
-        // skip duplicates for left & right
-        while (left < right && nums[left] === nums[left + 1]) left++;
-        while (left < right && nums[right] === nums[right - 1]) right--;
+        let l = i + 1;
+        let r = nums.length - 1;
 
-        left++;
-        right--;
-      } else if (sum < 0) {
-        left++;                      // need a larger sum
-      } else {
-        right--;                     // need a smaller sum
-      }
+
+        while (l < r) {
+
+            let sum = nums[l] + nums[r] + nums[i];
+
+            if (sum == 0) {
+                res.push([nums[i], nums[l], nums[r]]);
+
+                
+                while (l < r && nums[l] === nums[l + 1]) l++;
+                while (l < r && nums[r] === nums[r - 1]) r--;
+
+                l++;
+                r--;
+
+            }
+            else if (sum > 0) {
+                r--;
+            } else {
+                l++;
+            }
+
+        }
+
+
     }
-  }
-  return res;
+    return res;
 }
