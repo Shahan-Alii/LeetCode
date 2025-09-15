c13df5fd53f0128307ea018af829e5eebc9a8542
Shahan Ali <shahanali687@gmail.com>
2025-09-15 15:26:59 +0500

    Time: 13 ms (55.04%) | Memory: 63.6 MB (88.83%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.js
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.js
@@ -3,7 +3,7 @@ var numberOfSubarrays = function(nums, k) {
 };
 
 function countSubarrays(nums, k) {
-    let count = 0, start = 0, res = 0;
+    let  start = 0, res = 0;
 
     for (let end = 0; end < nums.length; end++) {
         if (nums[end] % 2 !== 0) k--;
