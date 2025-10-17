c9b24e06232b5b82b36660e88f039b3df46c4216
Shahan Ali <shahanali687@gmail.com>
2025-10-17 20:57:23 +0500

    Time: 1 ms (87.39%) | Memory: 64 MB (51.41%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+
+    let maxSum = nums[0];
+    let currSum = nums[0];
+
+    for (let i = 1; i < nums.length; i++) {
+
+        currSum = Math.max(nums[i], nums[i] + currSum)
+        maxSum = Math.max(maxSum, currSum);
+
+    }
+
+    return maxSum;
+
+};
